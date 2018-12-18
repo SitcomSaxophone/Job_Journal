120a8073e256181377a462c6c7d6dbee9deb7634
Samuel Solberg <pauliesays65@gmail.com>
2018-12-18 17:15:19 -0600

    checkbox with label added

--- src/components/JobItem/Jobitem.js
+++ src/components/JobItem/Jobitem.js
@@ -7,14 +7,26 @@ class JobItem extends Component {
         this.props.dispatch({ type: 'DELETE_JOB', payload: jobToDelete });
     }
 
+    handleJobChecked = jobToCheckOff => event => {
+        console.log(event.target.checked, jobToCheckOff);
+    }
+
     render() {
         return (
-            <>
+            <div>
                 <li key={this.props.job._id}>
-                    {this.props.job.company} {this.props.job.title} {this.props.job.dateAdded}
+                    <b>{this.props.job.company}</b> 
+                    <br/> 
+                    {this.props.job.title} 
+                    <br/> 
+                    {this.props.job.dateAdded}
+                    <br/>
                     <button onClick={this.handleDeleteJob(this.props.job)}>Remove Job</button>
+                    <input type="checkbox" defaultChecked={false} onChange={this.handleJobChecked(this.props.job)} name="CheckBox"/>
+                    <label htmlFor="CheckBox">Follow Up Sent?</label>
                 </li>
-            </>
+                <br/>
+            </div>
         )
     }
 }
