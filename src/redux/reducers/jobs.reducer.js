a4b1935a6f14df1ee7b093f8fd3ebe7ef8bbae35
Samuel Solberg <pauliesays65@gmail.com>
2018-12-18 16:53:38 -0600

    installed moment and formatted dates returned from database

--- src/redux/reducers/jobs.reducer.js
+++ src/redux/reducers/jobs.reducer.js
@@ -1,7 +1,11 @@
+import moment from 'moment';
+
 const jobs = (state = [], action) => {
     switch (action.type) {
         case 'SET_JOBS':
-            console.log(action.payload);
+        for (let i = 0; i < action.payload.length; i++) {
+            action.payload[i].dateAdded = moment(action.payload[i].dateAdded).format('L');
+        }
             return action.payload;
         default: 
             return state;
